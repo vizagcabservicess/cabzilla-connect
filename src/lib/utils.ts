899e2910802c8db90572b067a92e38748b253a7f
Naren <vizagcabservicess@gmail.com>
2025-06-15 22:45:10 +0530

    build errors fixed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,3 @@
-
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -16,3 +15,14 @@ export function slugify(text: string): string {
     .replace(/^-+/, '')             // Trim - from start of text
     .replace(/-+$/, '');            // Trim - from end of text
 }
+
+// Debounce function: delays invoking a function until after wait ms have elapsed since the last time it was invoked
+export function debounce<T extends (...args: any[]) => void>(func: T, wait: number): (...args: Parameters<T>) => void {
+  let timeout: ReturnType<typeof setTimeout> | undefined;
+  return function(this: any, ...args: Parameters<T>) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  };
+}
+
+export { formatPrice } from './cabData';
